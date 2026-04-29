cebc1705ea0ab38ec0791b46f83eaa0af4e81b8c
hamzademzz <hamzademir4772@hotmail.com>
2026-04-30 00:05:21 +0100

    fix error

---
+++ app/ilan-ver/page.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function IlanVer() {
+  return <div className="p-20 text-center">İlan Ver Sayfası</div>;
+}
--- app/profil/page.js
+++ app/profil/page.js
@@ -1,10 +1,4 @@
 import React from 'react';
-
-export default function Profiler() {
-  return (
-    <div className="p-10 text-center">
-      <h1 className="text-2xl font-bold">Profil Sayfası Yakında Burada Olacak!</h1>
-      <p className="mt-4">Şu an hazırlık aşamasındayız.</p>
-    </div>
-  );
+export default function Profil() {
+  return <div className="p-20 text-center">Profil Sayfası</div>;
 }
